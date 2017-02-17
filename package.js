31fc4792c74a16b9c3d33d6f7dc07520b754bb1e
Matt Mills <github@firefli.co.uk>
2017-02-17 21:10:32 +0000

    autoform update

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@1.0");
-  api.use(["jquery", "coffeescript", "templating", "aldeed:autoform@4.2.2"], "client");
+  api.use(["jquery", "coffeescript", "templating", "aldeed:autoform@5.8.1"], "client");
   api.addFiles([
   "lib/jQuery-Knob/js/jquery.knob.js",
   "afKnob.html",
